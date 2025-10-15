c7f6842a5368d584b4a6ce1b6c518b08c10f6113
Vishnuk_on_git <vkulkarni4ever@gmail.com>
2025-10-15 23:38:35 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.1"
+
+REPO_NAME = "E2E-MLFlow-Project-Data"
+AUTHOR_USER_NAME = "VIKINGVK"
+SRC_REPO = "mlProject"
+AUTHOR_EMAIL = "vishnukant2721@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for ml app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[]  # Add your dependencies here if any
+)
